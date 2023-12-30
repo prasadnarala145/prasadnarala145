ae0f7195b0fef4d14cf8b05fc09aee31180a7e15
prasadnarala145 <138651492+prasadnarala145@users.noreply.github.com>
2023-12-30 12:27:02 +0530

    Delete src directory

--- src/test/java/org/example/Rahw.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-import io.restassured.RestAssured;
-public class Rahw {
-    public static void main(String[] args) {
-
-        RestAssured.given().baseUri("https://restful-booker.herokuapp.com/ping")
-                .when().get().then().log().all().statusCode(201);
-    }
-}
